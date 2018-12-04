b51ceffeae2643626bfa7a3dd52a68240466fe1b
EmmMiranda <45522274+EmmMiranda@users.noreply.github.com>
2018-12-03 21:25:13 -0400

    Add files via upload
    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chp2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+        }
+    }
+}
